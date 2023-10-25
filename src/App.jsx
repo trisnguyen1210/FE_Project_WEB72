02c67bd54e168317fa7c31ff0f1fb64f7caf85ae
tuantran <anhkim280594@gmail.com>
2023-10-25 10:05:42 +0700

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme, Modal, Input, Typography } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const { Header, Sider, Content } = Layout;
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [login, setLogin] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const handleOk = () => {
+    setIsModalOpen(false);
+    setLogin(true)
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          style={{height: '100vh'}}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'Login',
+              onClick: () => {
+                setIsModalOpen(true);
+              }
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+        <Modal title="LogIn" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Typography>Tài Khoản:</Typography>
+        <Input placeholder='Nhập tài khoản'/>
+        <Typography>Mật khẩu:</Typography>
+        <Input placeholder='Nhập mật khẩn'/>
+      </Modal>
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+          Header
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
