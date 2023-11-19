b9d42ca56ad9c33abf7b7ca0bc834fef971aa446
TriNVM <trinvm1210@gmail.com>
2023-11-19 16:23:44 +0700

    update thumnail + fix warning

--- components/ContentPage/index.jsx
+++ components/ContentPage/index.jsx
@@ -3,26 +3,29 @@ import { getVideos } from "../../src/apis/mock-data/database";
 import { Card } from "antd";
 
 const ContentPage = (token) => {
-const [listVideos, setListVideos] = useState([]);
+  const [listVideos, setListVideos] = useState([]);
 
-useEffect(() => {
-   getVideos(token).then(res => setListVideos(res.videos))
-},[])
-  return(
+  useEffect(() => {
+    getVideos(token).then((res) => setListVideos(res.videos));
+  }, []);
+  return (
     <>
-    <Card style={{width:'fit-content'}}>
-            {listVideos.map((video) => {
-                return(
-                    <>
-                    <Card>
-                        <p>{video?.titleVideo}</p>
-                        <iframe width="100%" height="315" src={video.linkVideo} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    </Card>
-                    </>
-                )
-            })}
-    </Card>
+      <Card style={{ width: "fit-content" }}>
+        {listVideos.map((video) => (
+          <Card key={video._id}>
+            <p>{video?.titleVideo}</p>
+            <iframe
+              width="100%"
+              height="315"
+              src={video.linkVideo}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </Card>
+        ))}
+      </Card>
     </>
-  )
+  );
 };
 export default ContentPage;
